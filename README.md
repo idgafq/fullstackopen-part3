0a72df148fc7d60754e653d8dfa9c468fd0d27a6
Yang Song <154292645+idgafq@users.noreply.github.com>
2024-02-27 09:37:54 +0800

    submit part 3.9-3.11

---
+++ README.md
@@ -0,0 +1,1 @@
+backend: https://fullstackopen-part3-f5xe.onrender.com/api/persons
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('body', (req, res) => {
 	if (req.method === 'POST') {
@@ -9,6 +10,8 @@ morgan.token('body', (req, res) => {
 })
 
 const app = express()
+app.use(express.static('dist'))
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -100,5 +103,5 @@ const getRandomInt = (max) => {
 	return Math.floor(Math.random() * max)
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
